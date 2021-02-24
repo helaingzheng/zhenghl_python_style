7f3e2046396de3b28b54730a1ca5d9a2ac7407af
zhenghl0 <qpilihuo.110@qq.com>
2021-02-24 09:54:55 +0800

    修改了conf.py为google官方项目的conf（原来的复制了一份）

--- source/conf.py
+++ source/conf.py
@@ -13,41 +13,140 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+# -*- coding: utf-8 -*-
 
 
-# -- Project information -----------------------------------------------------
+import sys, os
+
 
 project = 'google_python_style'
 copyright = '2021, zheng'
 author = 'zheng'
+master_doc = 'index'   
 
 
-# -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = [
+version = u''
+release = u''
+
+source_suffix = '.rst'
+language = 'en_US'
+exclude_patterns = ['_build']
+extensions = ['sphinx.ext.imgmath']
+pygments_style = 'sphinx'
+
+# on_rtd is whether we are on readthedocs.org
+import os
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# otherwise, readthedocs.org uses their theme by default, so no need to specify it
+
+html_title = u'Google 开源项目风格指南'
+htmlhelp_basename = 'zh-google-styleguide'
+html_add_permalinks = ''
+
+latex_engine = 'xelatex'
+file_insertion_enabled = False
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if on_rtd:
+    latex_elements = {
+        # The paper size ('letterpaper' or 'a4paper').
+        #'papersize': 'letterpaper',
+        # The font size ('10pt', '11pt' or '12pt').
+        #'pointsize': '10pt',
+        # Additional stuff for the LaTeX preamble.
+        'preamble': r'''
+        \hypersetup{unicode=true}
+        \usepackage{CJKutf8}
+        \DeclareUnicodeCharacter{00A0}{\nobreakspace}
+        \DeclareUnicodeCharacter{2203}{\ensuremath{\exists}}
+        \DeclareUnicodeCharacter{2200}{\ensuremath{\forall}}
+        \DeclareUnicodeCharacter{2286}{\ensuremath{\subseteq}}
+        \DeclareUnicodeCharacter{2713}{x}
+        \DeclareUnicodeCharacter{27FA}{\ensuremath{\Longleftrightarrow}}
+        \DeclareUnicodeCharacter{221A}{\ensuremath{\sqrt{}}}
+        \DeclareUnicodeCharacter{221B}{\ensuremath{\sqrt[3]{}}}
+        \DeclareUnicodeCharacter{2295}{\ensuremath{\oplus}}
+        \DeclareUnicodeCharacter{2297}{\ensuremath{\otimes}}
+        \begin{CJK}{UTF8}{gbsn}
+        \AtEndDocument{\end{CJK}}    ''',
+    }
+else:
+    latex_elements = {
+        'papersize' : 'a4paper',
+        'utf8extra' : '',
+        'inputenc'  : '',
+        'babel'     : r'''\usepackage[english]{babel}''',
+        'preamble' : r'''        \usepackage{ctex}        ''',
+    }
+latex_documents = [
+  ('contents', 'zh-google-styleguide.tex', u'Google 开源项目风格指南',
+   u'', 'manual'),
 ]
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+#Add sponsorship and project information to the template context.
+context = {
+    'MEDIA_URL': "/media/",
+    'slug': 'google-styleguide',
+    'name': u'Google 开源项目风格指南',
+    'analytics_code': 'None',
+}
+
+html_context = context
 
 
-# -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-# html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+
+
+
+
+
+
+
+
+
+
+
+# # -- Project information -----------------------------------------------------
+
+# project = 'google_python_style'
+# copyright = '2021, zheng'
+# author = 'zheng'
+
+
+# # -- General configuration ---------------------------------------------------
+
+# # Add any Sphinx extension module names here, as strings. They can be
+# # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# # ones.
+# extensions = [
+# ]
+
+# # Add any paths that contain templates here, relative to this directory.
+# templates_path = ['_templates']
+
+# # List of patterns, relative to source directory, that match files and
+# # directories to ignore when looking for source files.
+# # This pattern also affects html_static_path and html_extra_path.
+# exclude_patterns = []
+
+
+# # -- Options for HTML output -------------------------------------------------
+
+# # The theme to use for HTML and HTML Help pages.  See the documentation for
+# # a list of builtin themes.
+# #
+# # html_theme = 'alabaster'
+# html_theme = 'sphinx_rtd_theme'
+
+# # Add any paths that contain custom static files (such as style sheets) here,
+# # relative to this directory. They are copied after the builtin static files,
+# # so a file named "default.css" will overwrite the builtin "default.css".
+# html_static_path = ['_static']
